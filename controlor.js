8a16642358b50f9b97c3ac600124ca611a95c64c
Ana Paula <anapaulacoqueirodogado@gmail.com>
2023-10-02 12:11:41 -0300

    atualizaçao js

--- controlor.js
+++ controlor.js
@@ -18,7 +18,22 @@ const LimparFormulario = (endereco) =>{
     document.getElementById('estado').value = endereco.uf;  
  }
  // Função para consumo de API utilizando a função do tipo assincrona
- const pesquisarcep = async() => {
+ const pesquisarCep = async() => {
     limparFormulario();
     const url = `http://viacep.com.br/ws/${cep.value}/json/`;
- }
+    if(cepValido(cep.value)){
+      const dados = await fetch(url);
+      const addres = await dados .json();
+    if(addres.hasOwnProperty('erro')){
+      alert('CEP não encontrado');
+    }else{
+         preencherFormulario(addres);
+       }
+      }else{
+         alert('CEP incorreto');
+      }
+    }
+   // Adiciona um evento DOM, no input CEP
+   document.getElementById('cep').addEventListener('focusout', pesquisarCep);
+
+ 
